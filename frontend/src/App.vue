f5a75a76ad8e8badde9a79576b7d866c37d31020
PUTTIPONG3035 <88504764+PUTTIPONG3035@users.noreply.github.com>
2023-04-09 01:02:59 +0700

    START AGAIN

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-
+    // HelloWorld
   }
 }
 </script>
@@ -20,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,9 +6,19 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Homes',
+    name: 'Home',
     component: () => import('../views/HomePage.vue') // set home as path '/'
   },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: () => import("../views/DetailPage.vue")
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import("../views/SearchPage.vue")
+  }
 ]
 
 const router = new VueRouter({
